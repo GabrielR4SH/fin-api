5648659e9d5e97fe9e8717e1cb155a9dac5d3f62
GabrielR4SH <bieelsantana23@hotmail.com>
2024-03-14 21:13:05 -0300

    Resource Add with Transictions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionsModule } from './transactions/transactions.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
+//Decorator
 @Module({
-  imports: [],
+  imports: [TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
