cedb1e27800318e82982b456baa46de3947909a5
root <root@DESKTOP-L619TH3>
2024-03-14 23:07:32 -0300

    Docker + Structure Done in Backend API

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionsModule } from './transactions/transactions.module';
-import { TransactionsModule } from './transactions/transactions.module';
 
 //Decorator
 @Module({
